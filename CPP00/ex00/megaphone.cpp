48798be960fb9e6b019ded717bd63f9d5016b699
Masha <myakoven@student.42berlin.de>
2025-01-18 18:19:47 +0100

    CPP0: Linux checked

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,34 +6,22 @@
 /*   By: myakoven <myakoven@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 22:55:25 by myakoven          #+#    #+#             */
-/*   Updated: 2024/12/28 05:00:15 by myakoven         ###   ########.fr       */
+/*   Updated: 2025/01/18 17:56:44 by myakoven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-char	*make_big(char *str)
+void	make_big(std::string &str)
 {
-	int		len;
-	// char	*new_str;
-	int		i;
-
-	len = std::strlen(str);
-	// new_str = new char[len + 2];
-	i = 0;
-	while (i < len)
-	{
-		// new_str[i] = (char)std::toupper(static_cast<unsigned char>(str[i]));
-		std::cout << (char)std::toupper(str[i]);
-		i++;
-	}
-	return (str);
+	for (size_t i = 0; i < str.length(); i++)
+		str[i] = static_cast<char>(std::toupper(static_cast<unsigned char>(str[i])));
+	return ;
 }
 
 int	main(int argc, char **argv)
 {
 	int i = 1;
-	// char *new_str;
 	if (argc < 2)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
@@ -41,11 +29,10 @@ int	main(int argc, char **argv)
 	}
 	while (argv[i])
 	{
-		// new_str = make_big(argv[i]);
-		// std::cout << new_str;
-		// delete[] new_str;
-		make_big(argv[i]);
+		std::string str = argv[i];
+		make_big(str);
 		i++;
+		std::cout << str;
 	}
 	std::cout << std::endl;
 	return (0);
--- CPP00/ex01/PhoneBook.class.cpp
+++ CPP00/ex01/PhoneBook.class.cpp
@@ -6,7 +6,7 @@
 /*   By: myakoven <myakoven@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 17:57:39 by myakoven          #+#    #+#             */
-/*   Updated: 2024/12/29 22:32:52 by myakoven         ###   ########.fr       */
+/*   Updated: 2025/01/17 14:19:45 by myakoven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,74 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream> //for stringstream
+
+/*PRIVATE*/
+
+void PhoneBook::updateStats()
+{
+	this->_updateOldest();
+	this->_updateLen();
+}
+
+/* works with a copy, not by reference */
+std::string PhoneBook::formatEntry(std::string entry)
+{
+	if (entry.length() > 10)
+		entry = entry.substr(0, 9) + ".";
+	return (entry);
+}
+
+size_t PhoneBook::getIndexFromUser(void)
+{
+	long int i;
+
+	std::string input;
+	// i = MAX_BOOK_LEN;
+	while (true)
+	{
+		std::cout << "Please enter the contact index: ";
+		if (!std::getline(std::cin, input))
+		{
+			if (std::cin.eof())
+			{
+				std::cin.clear();
+				std::cout << std::endl;
+				return (this->capacity);
+			}
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+			std::cout << "Invalid input, try again or write EXIT" << std::endl;
+			continue ;
+		}
+		if (input == "EXIT")
+			return (this->capacity);
+		std::stringstream ss(input);
+		if (!(ss >> i) || i < 0 || static_cast<size_t>(i) >= this->capacity)
+		{
+			std::cout << "Current entry:" << input << std::endl;
+			std::cout << INDEX_ERR << (this->capacity - 1) << std::endl;
+			continue ;
+		}
+		break ;
+	}
+	return (static_cast<size_t>(i));
+}
+
+void PhoneBook::_updateOldest()
+{
+	if (this->_len == 8)
+		_oldestIndex = (_oldestIndex + 1) % 8;
+	return ;
+}
+
+void PhoneBook::_updateLen()
+{
+	if (this->_len < 8)
+		this->_len += 1;
+	return ;
+}
+
+/*PUBLIC */
 
 PhoneBook::PhoneBook() : capacity(MAX_BOOK_LEN), _len(0), _oldestIndex(0)
 {
